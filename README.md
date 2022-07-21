2333f6467d3e3580d2e9abc8ebc775e3b87c65b4
Julien Adam <adamj@paratools.com>
2022-07-21 08:32:27 +0200

    Update

---
+++ README.md
@@ -0,0 +1,6 @@
+README
+======
+
+
+* Build the container: `docker build -t pcvs-training ./docker`
+* Run the container: `docker run -it pcvs-training`
--- resources/mpi/test.c
+++ resources/mpi/test.c
@@ -1,4 +1,5 @@
 #include <mpi.h>
+#include <stdio.h>
 
 int main( int argc, char *argv[])
 {
@@ -7,7 +8,7 @@ int main( int argc, char *argv[])
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-        printf("Rank: %d/%d\n", rank, size);
+	printf("Rank: %d/%d\n", rank, size);
 
 	MPI_Finalize();
 
